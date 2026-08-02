820e8f214b96d4b824eeb6be297ad104eae8368a
Heran Tesfaye <heran.teslem@gmail.com>
2026-08-02 12:08:03 +0300

    create homepage

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+console.log('Question 1')
+console.log(2>4);
+console.log(24 > 3);
+console.log(2 < "12");
+console.log(0 == 2);
+console.log(2.0 === 2);
+console.log(2.0 == "2");
+console.log(2 < "John");
+console.log(2 > "John");
+console.log("2" < "2");
+console.log("2" > "12");
+console.log(1 == 1 || 3 == 2 || 3 == 7);
+console.log(1 == 1 && 2 == 2 && 3 == 7)
+console.log(1 == 1 || 2 == 2 && 3 == 7)
+console.log(1 == true && 0 > true || "31" > "9" || 10 > 5 || !("2" == "two" || 1 == "1"));
+console.log('Question 2')
+console.log('D');
+console.log('B');
+console.log('D');
+console.log('B');
+console.log('A');
+console.log('D');
+console.log(+"2"+2);
+console.log(7 % 3);
+console.log(2+true);
+console.log('Question 3')
+let a=1;
+let b=2;
+let c=a+b;
+console.log(c)
+console.log('Question 4')
+let firstName="Heran";
+let lastName="Tesfaye";
+let fullName=firstName+ " " + lastName;
+console.log(fullName);
